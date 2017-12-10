f9e23ec5a6bdd9ba405f5e6f82e3b2e049e3011b
anishbnair <nbanish@gmail.com>
2017-12-10 17:53:10 -0500

    created a function to create unique random values for each crystal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,72 @@
 // 
 // Create a variable to store sytem generated random number between 19 - 120
-// Create a variable to store value of each crystal between 1 - 12 
-// 
+// Create variable to store value of each crystal between 1 - 12 
 // System selects a randon number and dispay in the screen
+// Assign unique random value to each crystal 
 
 // VARIABLES
 // ====================================================================================================================================
 var randNum;
-var crystalValue;
+//var crystalValue = [];
+var redCrystal = 0;
+var blueCrystal = 0;
+var yellowCrystal = 0;
+var greenCrystal = 0;
+var wins = 0;
+var losses = 0;
 
 
 // Store the sytem generated random number between 19 - 120 to variable 
 randNum = Math.floor((Math.random() * 100) + 19);
 console.log("System generated Random Number is " + randNum);
 
-// Store the sytem generated random number between 19 - 120 to variable 
-crystalValue = Math.floor((Math.random() * 12) + 1);
-console.log("Crystal value is " + crystalValue);
+// Display random number on the screen
+$(".guessNum").append(randNum);
+
+// Assign random value to each crystal  
+// redCrystal = Math.floor((Math.random() * 12) + 1);
+// console.log("Red Crystal value is " + redCrystal);
+// blueCrystal = Math.floor((Math.random() * 12) + 1);
+// console.log("Blue Crystal value is " + blueCrystal);
+// yellowCrystal = Math.floor((Math.random() * 12) + 1);
+// console.log("Yellow Crystal value is " + yellowCrystal);
+// greenCrystal = Math.floor((Math.random() * 12) + 1);
+// console.log("Green Crystal value is " + greenCrystal);
+// for (i = 0; i < 4; i++) {
+//     crystalValue[i] = Math.floor((Math.random() * 12) + 1);
+//     console.log(crystalValue[i]);
+//     //console.log(crystalValue[i]);
+// };
+// redCrystal = crystalValue[0];
+// console.log("Red Crystal value is " + redCrystal);
+// blueCrystal = crystalValue[1];
+// console.log("Blue Crystal value is " + blueCrystal);
+// yellowCrystal = crystalValue[2];
+// console.log("Yellow Crystal value is " + yellowCrystal);
+// greenCrystal = crystalValue[3];
+// console.log("Green Crystal value is " + greenCrystal);
+
+function generateCrystalValue() {
+    var max = 12;
+    var crystalValue = [];
+    for (var i = 0; i < max; i++) {
+        var temp = Math.floor(Math.random() * max);
+        if (crystalValue.indexOf(temp) == -1) {
+            crystalValue.push(temp);
+        }
+        else
+            i--;
+    }
+    console.log(crystalValue);
+    redCrystal = crystalValue[0];
+    console.log("Red Crystal value is " + redCrystal);
+    blueCrystal = crystalValue[1];
+    console.log("Blue Crystal value is " + blueCrystal);
+    yellowCrystal = crystalValue[2];
+    console.log("Yellow Crystal value is " + yellowCrystal);
+    greenCrystal = crystalValue[3];
+    console.log("Green Crystal value is " + greenCrystal);
+}
+generateCrystalValue();
+
 
